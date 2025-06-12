230b8b180f5b08cf4837ad846468a5c4882f07e8
aharsha7 <aharsha7na@gmail.com>
2025-06-12 12:03:31 +0530

    fixx

--- server.js
+++ server.js
@@ -2,29 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mailRoutes = require('./routes/mailRoutes');
 const path = require('path');
-const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
-
-// Serve static files (if needed)
 app.use(express.static('public'));
-
-// ✅ CORS: allow frontend domain
-app.use(cors({
-  origin: 'https://frontend-mail-eta.vercel.app',
-  methods: ['GET', 'POST'],
-  credentials: true,
-}));
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// API Routes
 app.use('/api/mail', mailRoutes);
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
